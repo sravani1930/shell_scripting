91b48802c8a245038f9ccb0993bdddbc7adaba21
sravani192019 <sravani.jamithireddy@gmail.com>
2025-04-02 03:31:00 +0530

    logs script

--- 11-logs.sh
+++ 11-logs.sh
@@ -1,29 +1,38 @@
 #!/bin/bash
 ID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOGFILE="/tmp/$0_$TIMESTAMP.log"
+
+echo "script started executing at $TIMESTAMP" &>> $LOGFILE
+
+R="\e[31m"
+G="\e[32m"
+y="\e[33m"
+N="\e[0m"
 
 VALIDATE(){
    if [ $1 -ne 0 ]
    then
-       echo " ERROR :: $2 ... FIALED !!!!"
+       echo -e " $R ERROR :: $2 ... FIALED !!!! $N"
        exit 1
    else
-       echo " $2 SUCCESS !!!"
+       echo  -e" $2 ....$G SUCCESS !!! $N"
    fi
 }
 
 if [ $ID -ne 0 ]
 then
-   echo " please switch to root user"
+   echo  -e " $R please switch to root user $N"
    exit 1
 else
    echo "proceed to install the package"
 fi
 
-yum install mysql -y
+yum install mysql -y &>> $LOGFILE
 
 VALIDATE $? "MY SQL INSTALLATION "
 
-yum install git -y
+yum install git -y &>> $LOGFILE
 
 VALIDATE $? "GIT INSTALLATION"
 
