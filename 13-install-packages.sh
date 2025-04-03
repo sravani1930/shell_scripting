e3187eeff6b09e29d1549a539590d14c54c9748c
sravani192019 <sravani.jamithireddy@gmail.com>
2025-04-04 00:15:04 +0530

    package installation using loops

--- 13-install-packages.sh
+++ 13-install-packages.sh
@@ -1,12 +1,43 @@
 #!/bin/bash
 ID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOGFILE="/tmp/$0_$TIMESTAMP.log"
+
+echo "script started executing at $TIMESTAMP" &>> $LOGFILE
+
+R="\e[31m"
+G="\e[32m"
+y="\e[33m"
+N="\e[0m
+
+validate(){
+   if [ $1 -ne 0 ]
+   then
+      echo -e "$R ERROR :: $2 .. FAILED $N"
+      exit 1;
+   else
+      echo -e "$G $2 ... SUCCESS $N"
+   fi
+}
 
 if [ $ID -ne 0 ]
 then
-   echo " please switch to root user"
+   echo -e "$R please switch to root user $N"
    exit 1
 else
    echo "proceed to install the package"
 fi
 
-echo "All are=guments passed :$@"
+for package in $@
+do
+  yum list installed $package
+  if [ $? -ne 0 ]
+  then
+      yum install $package &>> $LOGFILE
+      validate $? "$package INSTALLATION "
+   else
+      echo -e "$Y $Package is already Installed !!!..SKIPPING $N"
+      
+   fi
+done
+    
