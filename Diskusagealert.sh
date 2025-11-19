a79e4272ef9b2d02e369548e6004f0b020980abe
sravani192019 <sravani.jamithireddy@gmail.com>
2025-11-19 19:36:29 +0530

    Disk usage

--- Diskusagealert.sh
+++ Diskusagealert.sh
@@ -1,47 +1,21 @@
 #!/bin/bash
-ID=$(id -u)
-TIMESTAMP=$(date +%F-%H-%M-%S)
-LOGFILE="/tmp/$0_$TIMESTAMP.log"
-echo "script started executing at $TIMESTAMP" &>> $LOGFILE
 
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-N="\e[0m"
-
-if [ $ID -ne 0 ]
-then
-   echo -e "$R please switch to root user $N"
-   exit 1
-else
-   echo "proceed to check diskusage"
-fi
-
-DISK_USAGE=$(df -hT | grep -vE 'tmp|file|Filesystem')
+DISK_USAGE=$(df -hT | grep -vE 'tmp|File')
 DISK_THRESHOLD=1
-MESSAGE=""
+message=""
 
-while IFS= read -r line
+while IFS= read line
 do
-    usage=$(echo $line | awk '{print $7F}' | tr -d '%')
-    DISK_NAME=$(echo $line | awk '{print $1F}')
-    if [ $usage -gt $DISK_THRESHOLD ] 
+    usage=$(echo $line | awk '{print $6F}' | cut -d % -f1)
+    partition=$(echo $line | awk '{print $1F}')
+    if [ $usage -ge $DISK_THRESHOLD ]
     then
-        MESSAGE+="High usage alert on $DISK_NAME: ${USAGE}%\n"
+        message+="High Disk Usage on $partition: $usage <br>"
     fi
 done <<< $DISK_USAGE
 
-echo -e "message:\n$MESSAGE"
+echo -e "Message: $message"
 
+#echo "$message" | mail -s "High Disk Usage" info@joindevops.com
 
-
-# if [ $DISKUSAGE -ge 10%]
-# then
-#     mail -s "Disk usage reaches more than 10%" sravani3093@example.com <<EOF
-# Hi Team,
-# This is to inform you below Filesystem reached more than 10%
-# please perform the houskeeping to limit the filesystem thereshold
-
-# Thanks & Regards,
-# Monitoring Team
-# EOF
+#sh mail.sh "DevOps Team" "High Disk Usage" "$message" "info@joindevops.com" "ALERT High Disk Usage"
