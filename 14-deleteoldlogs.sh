bdfa5b495596a528f68fcc1876beffe7ab8791f4
sravani192019 <sravani.jamithireddy@gmail.com>
2025-11-19 16:04:50 +0530

    Deleting logfiles

---
+++ 14-deleteoldlogs.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+ID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOGFILE="/tmp/$0_$TIMESTAMP.log"
+
+if [ $ID -ne 0 ] {
+then
+   echo " please switch to root user"
+   exit 1
+else
+   echo "proceed to delete package"
+fi
+
+}
+
+
+find /var/log -type f -name "*.log" -mtime +15 &>> $LOGFILE
+if [ $? -ne 0] {
+then
+    echo " There are no log files available more than 15 days "
+    exit 1
+else
+    echo "LOGFILES more than 15 days has been removed"   
+}
