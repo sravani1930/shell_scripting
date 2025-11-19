c3fdf1e386aba792970bedf2098fbfa06c86367b
sravani192019 <sravani.jamithireddy@gmail.com>
2025-11-19 20:00:34 +0530

    Disk usage

--- Diskusagealert.sh
+++ Diskusagealert.sh
@@ -15,48 +15,7 @@ do
 done <<< $DISK_USAGE
 
 echo -e "Message: $message"
-FROM="sravani3093@gmail.com"
-TO="sravani.jamithireddy@gmail.com"
-SUBJECT="Disk Usage Alert on Server $(hostname)"
-EMAIL_TEXT="/tmp/email_body_$$.txt"
-
-cat <<EOF > "$EMAIL_TEXT"
-Hello Team,
-
-Please find the disk usage alert details below:
-
-$message
-
-Kindly perform housekeeping to free up disk space.
-
-Thanks & Regards,
-Linux Admin
-EOF
-
-EMAIL_JSON="/tmp/email_json_$$.json"
-
-cat <<EOF > "$EMAIL_JSON"
-{
-  "Source": "$FROM",
-  "Destination": {
-    "ToAddresses": ["$TO"]
-  },
-  "Message": {
-    "Subject": {
-      "Data": "$SUBJECT"
-    },
-    "Body": {
-      "Text": {
-        "Data": "$(sed 's/"/\\"/g' "$EMAIL_TEXT")"
-      }
-    }
-  }
-}
-EOF
-
-aws ses send-email --cli-input-json file://$EMAIL_JSON
-
-rm -f "$EMAIL_TEXT" "$EMAIL_JSON"
+sh mail.sh "DevOps Team" "High Disk Usage" "$message" "sravani3093@gmail.com" "ALERT High Disk Usage"
 
 
 
