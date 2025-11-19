033d201cc76e380fc893161779fddb7fb295bc0a
sravani192019 <sravani.jamithireddy@gmail.com>
2025-11-19 16:07:00 +0530

    Deleting logfiles

--- 14-deleteoldlogs.sh
+++ 14-deleteoldlogs.sh
@@ -10,11 +10,9 @@ then
 else
    echo "proceed to delete package"
 fi
-
 }
 
-
-find /var/log -type f -name "*.log" -mtime +15 &>> $LOGFILE
+find /var/log/ -type f -name "*.log" -mtime +15 &>> $LOGFILE
 if [ $? -ne 0] {
 then
     echo " There are no log files available more than 15 days "
